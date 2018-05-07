fb8e189d67f3bed04ae3658705d67fd159163b6f
Christian Böhm <gh_hz555@hotmail.com>
2018-05-07 15:12:19 +0200

    Hello World BC

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            var xs = new [] {1,2,3};
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
