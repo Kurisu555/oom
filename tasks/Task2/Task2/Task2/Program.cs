7b3c23559b8e17e7a8fca6e46ec3531565127c61
Christian Böhm <gh_hz555@hotmail.com>
2018-05-15 14:46:59 +0200

    Task1

---
+++ tasks/Task2/Task2/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song R_as_randome = new Song("Amaranthe", "Mechanical Illusion", 1);
+
+            Console.WriteLine(R_as_randome.GetTitleAndInterpreter());
+            Console.WriteLine(R_as_randome.GetPlayOutput());
+            R_as_randome.PressPlayButton();
+            Console.WriteLine(R_as_randome.GetPlayOutput());
+            R_as_randome.PressPlayButton();
+            Console.WriteLine(R_as_randome.GetPlayOutput());
+
+        }
+    }
+
+    public class Song
+    {
+        private String title;
+        private String interpreter;
+        public int PlaceNumber;
+        public bool play = true;
+
+
+        public Song(String title, String interpreter, int PlaceNumber = 0)
+        {
+            this.title = title;
+            this.interpreter = interpreter;
+            this.PlaceNumber = PlaceNumber;
+        }
+
+        public void SetPlaceNumber(int PlaceNumber)
+        {
+            this.PlaceNumber = PlaceNumber;
+        }
+
+        public String GetTitleAndInterpreter()
+        {
+            return title + " - " + interpreter + "at place Number" + PlaceNumber;
+        }
+
+        public void PressPlayButton()
+        {
+            if (play == false) play = true;
+            else play = false;
+        }
+        public String GetPlayOutput()
+        {
+            if (play == false) return "stop";
+            return "play";
+        }
+    }
+    
+}
